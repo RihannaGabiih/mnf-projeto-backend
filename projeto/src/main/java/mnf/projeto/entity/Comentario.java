d2785c2c447d216559caa9470de616f669429bdf
Kelwin-max <kelwingilfernandes18@gmail.com>
2025-12-05 09:11:53 -0300

    feat: added getters and setters

--- projeto/src/main/java/mnf/projeto/entity/Comentario.java
+++ projeto/src/main/java/mnf/projeto/entity/Comentario.java
@@ -20,5 +20,36 @@ public class Comentario {
 
     private String texto;
     private LocalDateTime dataComentario;
-
+	public Long getIdComentario() {
+		return idComentario;
+	}
+	public void setIdComentario(Long idComentario) {
+		this.idComentario = idComentario;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Noticia getNoticia() {
+		return noticia;
+	}
+	public void setNoticia(Noticia noticia) {
+		this.noticia = noticia;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDateTime getDataComentario() {
+		return dataComentario;
+	}
+	public void setDataComentario(LocalDateTime dataComentario) {
+		this.dataComentario = dataComentario;
+	}
+    
+    
 }
