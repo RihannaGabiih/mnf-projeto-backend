549b456adec75a7e3d8c252167adcf94468c4841
RicardoRibeiroTri <ricardoribeirotrindade273@gmail.com>
2025-12-07 22:02:34 -0300

    test

--- projeto/src/main/java/mnf/projeto/repositories/DenunciaRepository.java
+++
@@ -1,8 +0,0 @@
-package mnf.projeto.repositories;
-
-import mnf.projeto.entity.Denuncia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
-    
-}
